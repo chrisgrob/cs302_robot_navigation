08528d0d834029435cff9b46ae25895cc2fefc4f
Gage Christensen <grchr4000@gmail.com>
2019-11-16 23:56:35 -0800

    made some new classes for the purpose of ray tracing

--- robot.h
+++ robot.h
@@ -1,12 +1,32 @@
 #pragma once
 #ifndef ROBOT_H
 #define ROBOT_H
-#include "map.cpp"
+// #include "map.cpp" Doesn't exist? - Gage
 #incldue <iostream>
 #include <cstdlib>
 #include <vector>
+#include <cmath>
 
+const double pi = 3.14159265358979323846
 
+// An x coord, a y coord, and a probability of being occupied
+class Point {
+	public:
+		const int x;
+		const int y;
+		const float occupied;
+
+		Point(int x, int y, float occupied);
+}
+
+class Ray {
+	private:
+		std::vector<Point> points;
+	public:
+		Ray(int direction, Map map);
+
+		std::vector<Point> get_points();
+}
 
 class Robot {
   private:
@@ -22,8 +42,10 @@ class Robot {
 	bool ableToMove();
 	void rotate();
 	void move(); //Moves one space
-    bool ray_casting();
+  std::vector<Point> ray_casting();
 	
+	private:
+		
 };
 Robot::Robot()
 {
@@ -94,8 +116,27 @@ void Robot::move()
 		locX - 1;
 	}
 }
-bool Robot::ray_casting() //Hey Gage do this
+std::vector<Point> Robot::ray_casting() //Hey Gage do this
 {
 	return false;
 }
+
+// I hate trig
+Ray::Ray(const int direction, const Map map, const int robot_x, const int robot_y)
+{
+	const int radians = (direction * 2 * pi) / 360;
+
+	std::vector points;
+
+	int x_read = robot_x;
+	int y_read - robot_y;
+	bool occupied = false;
+	while (!occupied) {
+		x_read_to = x_read + 1;
+		y_read_to = y_read + tan(radians);
+
+		
+	}
+}
+
 #endif //ROBOT_H
