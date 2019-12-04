d6d8403a298c7dd39ce88bffee4cdc10fbbd7457
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-04 15:06:32 -0800

    Update robot.h

--- headers/include/robot.h
+++ headers/include/robot.h
@@ -4,39 +4,40 @@
 #include "occupancy_grid_map.h"
 #include "robot_utils.h"
 
-const double pi = 3.14159265358979323846;
+const double pi = 3.14159265358979323846; //value of pi
 
 class Robot
 {
 private:
-  CardinalDirection orientation_;
-  CoordinateType pos_;
-  OccupancyGridMap map_;
+  CardinalDirection orientation_; //Where the robot is currently facing
+  CoordinateType pos_; //The position on the map that it is currently in
+  OccupancyGridMap map_; //The actual map that the robot is on
 
 public:
-  Robot();
-  Robot(const CoordinateType pos, const CardinalDirection orientation, const OccupancyGridMap map);
+  Robot(); //default ctor
+  Robot(const CoordinateType pos, const CardinalDirection orientation, const OccupancyGridMap map); //parametrized ctor
 
 
-  void set_pos(const CoordinateType pos);
-  CoordinateType get_pos() const;
+  void set_pos(const CoordinateType pos); //sets position of the robot
+  CoordinateType get_pos() const; //returns the current position of the robot
 
-  void set_orientation(const CardinalDirection orientation);
-  CardinalDirection get_orientation() const;
+  void set_orientation(const CardinalDirection orientation); //sets orientation of the robot
+  CardinalDirection get_orientation() const; //Returns the current orientation of the robot
 
-  void set_map(const OccupancyGridMap map);
-  const OccupancyGridMap& get_map() const;
+  void set_map(const OccupancyGridMap map); //Sets the current map of the robot
+  const OccupancyGridMap& get_map() const; //Returns what map the robot is on
   
 
-  void RayCasting();
+  void RayCasting(); //The method to actually raycast depending on the position of the robot
 
 private:
-  void RayRange(const int start, const int end);
-  void Ray(const int direction);
-  void Cast(const SignPair steps, const int direction);
-  void CastVertical(const SignPair steps);
-  void CastDiagonal(const SignPair steps);
+  //The following are helper methods for RayCasting
+  void RayRange(const int start, const int end); //Determines the range of the cast
+  void Ray(const int direction); //What the ray is going to be
+  void Cast(const SignPair steps, const int direction); //where the case is going to take place
+  void CastVertical(const SignPair steps); //Vertically casts depending on the amount of steps
+  void CastDiagonal(const SignPair steps); //Same but with diagonal
 
-  void UpdateMap(VertexType vertex, double sensor_reading);
+  void UpdateMap(VertexType vertex, double sensor_reading); //Updates the map depending on the vertex and the sensor reading of the vertex
 
 };
