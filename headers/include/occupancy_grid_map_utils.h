3671bc677cbf82f2c1f8cd558614b2254affb585
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-04 15:00:07 -0800

    Update occupancy_grid_map_utils.h

--- headers/include/occupancy_grid_map_utils.h
+++ headers/include/occupancy_grid_map_utils.h
@@ -2,8 +2,8 @@
 
 #include "project_utils.h"
 
-bool VertexIsObstacle(const VertexType vertex, const Obstacle obstacle, const int map_size);
-bool VertexIsTop(const VertexType vertex, const int map_size);
-bool VertexIsBottom(const VertexType vertex, const int map_size);
-bool VertexIsLeftEdge(const VertexType vertex, const int map_size);
-bool VertexIsRightEdge(const VertexType vertex, const int map_size);
+bool VertexIsObstacle(const VertexType vertex, const Obstacle obstacle, const int map_size); //Checks if the vertex is an obstacle
+bool VertexIsTop(const VertexType vertex, const int map_size); //Checks if the vertex is at the top of the map
+bool VertexIsBottom(const VertexType vertex, const int map_size); //Checks if vertex is at the bottom of the map
+bool VertexIsLeftEdge(const VertexType vertex, const int map_size); //Checks if vertex is at the left edge of the map
+bool VertexIsRightEdge(const VertexType vertex, const int map_size); //Cehcks if vertex is at the right edge of the map
