a3cfca7586e53163517c66f70735339823c5c16f
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-04 14:59:08 -0800

    Update occupancy_grid_map.h

--- headers/include/occupancy_grid_map.h
+++ headers/include/occupancy_grid_map.h
@@ -7,8 +7,8 @@
 class OccupancyGridMap
 {
 private:
-  MapType map_;
-  const int map_size_;
+  MapType map_; //Generates a type map
+  const int map_size_;  //Size of the map, by default should be 10000
 
 public:
   OccupancyGridMap();
@@ -16,31 +16,31 @@ public:
   OccupancyGridMap(const OccupancyGridMap& other);
 
 
-  int VertexCount() const;
-  int EdgeCount() const;
+  int VertexCount() const; //Gets the vertex count for the total Map
+  int EdgeCount() const; //Edge count for the map
 
 
-  void set_map(const MapType map);
-  const MapType& get_map() const;
+  void set_map(const MapType map); //Sets the map to be equal to the other map
+  const MapType& get_map() const;//Gets the map
 
-  int get_map_size() const;
-
-
-  VertexType Vertex(const CoordinateType pos) const;
-
-  CoordinateType Coordinate(const VertexType vertex) const;
+  int get_map_size() const; //returns map size
 
 
-  bool IsInBounds(const int x_pos, const int y_pos) const;
+  VertexType Vertex(const CoordinateType pos) const;//Returns a vertex based off of the coordinate position
+
+  CoordinateType Coordinate(const VertexType vertex) const; //Returns a coordinate based off of a vertex
 
 
-  void UpdateMap(const int x_pos, const int y_pos);
+  bool IsInBounds(const int x_pos, const int y_pos) const; //Checks to see if the position is in bounds
+
+
+  void UpdateMap(const int x_pos, const int y_pos); //Updates map based off of the x and y position
 
 
   void Visualize() const;
-  friend std::ostream& operator<<(std::ostream& os, const OccupancyGridMap& map);
+  friend std::ostream& operator<<(std::ostream& os, const OccupancyGridMap& map); //Outputs the map when using os << map
 
 private:
-  void Visualize(std::ostream& os) const;
+  void Visualize(std::ostream& os) const; //Visualize the map 
 
 };
