55dfcd5f5b1f8303fc747bc796e9577070556d1c
dahneal <56851005+dahneal@users.noreply.github.com>
2019-12-04 15:08:26 -0800

    Update sign.h

--- headers/include/sign.h
+++ headers/include/sign.h
@@ -1,6 +1,6 @@
 #pragma once
 
-enum Sign {
+enum Sign { //Simple enum to help determine if the number is positive or negative, based off of previous quadrants on the map
   Positive = 1,
   Negative = -1,
 };
