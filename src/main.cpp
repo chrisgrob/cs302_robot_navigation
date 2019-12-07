cbb1b8f866e8efe90dcdafa82dd201ca690ffbec
Gage Christensen <grchr4000@gmail.com>
2019-12-06 17:41:10 -0800

    actual problem specification is set up

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,64 @@
 
 int main()
 {
-  const IndexType map_size = 16;
-  const Obstacle obstacle = Obstacle(0, 3, 0, 3);
+  const IndexType map_size = 1000;
+  const Obstacle obstacle = Obstacle(249, 499, 249, 499);
 
+  std::cout << "Map is being set up" << std::endl;
   OccupancyGridMap robot_map = OccupancyGridMap(map_size, obstacle);
+  std::cout << "Map is done being set up" << std::endl;
 
-  Robot robot = Robot(std::make_pair(7, 7), CardinalDirection::West, robot_map);
-
+  // 1
+  Robot robot = Robot(std::make_pair(0, 0), CardinalDirection::East, robot_map);
+  robot.RayCasting();
+  
+  // 2
+  robot.set_pos(std::make_pair(156, 147));
+  robot.set_orientation(CardinalDirection::East);
+  robot.RayCasting();
+  
+  // 3
+  robot.set_pos(std::make_pair(76, 273));
+  robot.set_orientation(CardinalDirection::East);
+  robot.RayCasting();
+  
+  // 4
+  robot.set_pos(std::make_pair(69, 421));
+  robot.set_orientation(CardinalDirection::East);
+  robot.RayCasting();
+  
+  // 5
+  robot.set_pos(std::make_pair(72, 528));
+  robot.set_orientation(CardinalDirection::East);
+  robot.RayCasting();
+  
+  // 6
+  robot.set_pos(98, 678);
+  robot.set_orientation(CardinalDirection::East);
+  robot.RayCasting();
+  
+  // 7
+  robot.set_pos(377, 606);
+  robot.set_orientation(CardinalDirection::North);
+  robot.RayCasting();
+  
+  // 8
+  robot.set_pos(657, 434);
+  robot.set_orientation(CardinalDirection::West);
+  robot.RayCasting();
+  
+  // 9
+  robot.set_pos(682, 215);
+  robot.set_orientation(CardinalDirection::North);
   robot.RayCasting();
 
   std::cout << robot.get_map() << std::endl;
   std::cout << std::endl << std::endl;
+  
+  // 10th measurement
+  robot.set_pos(999, 999);
+  robot.set_orientation(CardinalDirectoin::North);
+  robot.RayCasting();
 
   return 0;
 }
